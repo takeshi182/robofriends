f3b478eb953ab4aa48421dabb232d0440dc7ea4f
Takeshi182 <hataketakeshi725@gmail.com>
2021-05-17 19:33:39 +0430

    Created new Component

---
+++ robofriends/src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const Card = () => {
+    return (
+        <div>
+            <img src="https://robohash.org/test?100*100" alt="robot" />
+            <div>
+                <h2>Jane Doe</h2>
+                <p>jane.doe@gmail.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
-
+import 'tachyons';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Card />
+    <Card />
+    <Card />
   </React.StrictMode>,
   document.getElementById('root')
 );
