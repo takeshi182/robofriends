adcec1186dd9e1321b4babc62657f6a6bd22a5ff
Takeshi182 <hataketakeshi725@gmail.com>
2021-05-17 19:56:43 +0430

    Added to the new Component

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,11 +1,11 @@
 import React from 'react';
-const Card = () => {
+const Card = ({name, email, id}) => {
     return (
-        <div>
-            <img src="https://robohash.org/test?100*100" alt="robot" />
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img src={`https://robohash.org/${id}`} alt="robot" />
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     )
