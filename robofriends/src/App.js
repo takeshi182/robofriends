8ac5c6ef7dc7cf228370d0e0c03fe16160e73a5f
Takeshi182 <hataketakeshi725@gmail.com>
2021-06-01 12:54:37 +0430

    Finished the project

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import {robots} from './robots';
 import SearchBox from './SearchBox'
+import Scroll from './Scroll'
 
 class App extends Component {
     constructor() {
         super() // when extending a class, super() must always come before this.state
         //state lives in the parent component
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ""
         } //state is the thing that will affect our input (robots in this case) based on sth else (searchfield in this case)
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users').then(response=>response.json()).then(users => this.setState({robots:users}));
     }
 
     onSearchChange = (event) => {
@@ -25,7 +29,9 @@ class App extends Component {
             <div className="tc">
                 <h1>Robofriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/> 
+                <Scroll>
+                    <CardList robots={filteredRobots}/> 
+                </Scroll>
             </div>
         )
     }
